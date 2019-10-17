170b5624f3a1147f0cabb4fe9ee2bc396a78d179
Arthur De Witte <arthur.dewitte@gmail.com>
2019-10-17 21:11:57 +0200

    Fixed custom prefix

--- extensions/Main.py
+++ extensions/Main.py
@@ -5,6 +5,10 @@ class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="test")
+    async def test(self, ctx):
+        await ctx.send("hoi")
+
 
 def setup(bot):
     bot.add_cog(Main(bot))
--- util/core/data.py
+++ util/core/data.py
@@ -1,6 +1,7 @@
 import sqlite3
 import configparser
 import datetime
+import json
 from discord.ext import commands
 
 commandConfig = configparser.ConfigParser()
@@ -11,20 +12,35 @@ commandConfig.read('config.cfg')
 #################
 
 
-def get_prefix(bot, message):
+def get(bot, message, default):
     connect = sqlite3.connect(commandConfig["DATABASE"]["utilityDB"])
     cursor = connect.cursor()
     try:
         cursor.execute("SELECT * FROM prefixes")
     except sqlite3.OperationalError:
         cursor.execute("CREATE TABLE prefixes (server_id int, prefix text)")
-    try:
-        output = cursor.execute(f"SELECT prefix FROM prefixes WHERE server_id = {message.guild.id}")
-    except sqlite3.OperationalError:
-        output = "!"
+    db_output = cursor.execute(f"SELECT prefix FROM prefixes WHERE server_id = {message.guild.id}")
+    if len(db_output.fetchall()) == 0:
+        output = default
+    else:
+        output = ['<@634141001769943090> ',
+                  '<@!634141001769943090> ',
+                  '<@634141001769943090>',
+                  '<@!634141001769943090>']
+        output += db_output.fetchall()
     connect.commit()
     connect.close()
     return output
+
+
+def get_prefix(bot, message):
+    output = str(commandConfig.get("UTILITY", "defaultPrefixes")).split(",") + ['<@634141001769943090> ',
+                                                                                '<@!634141001769943090> ',
+                                                                                '<@634141001769943090>',
+                                                                                '<@!634141001769943090>']
+    if not message.guild:
+        return commands.when_mentioned_or(*output)(bot, message)
+    return commands.when_mentioned_or(*get(bot, message, output))(bot, message)
 
 
 ###################
@@ -41,9 +57,11 @@ def add_command(server_id: int, author, name: str, response: str):
     except sqlite3.OperationalError as e:
         cursor.execute("CREATE TABLE servers (date blob, server_id int, creator blob, name text, response text)")
     parameters = (datetime.datetime.now(), server_id, author, name, response)
-    cursor.execute("INSERT INTO servers (?, ?, ?, ?, ?)", parameters)
+    command_info = cursor.execute("INSERT INTO servers (?, ?, ?, ?, ?)", parameters)
     connect.commit()
     connect.close()
+    return command_info.fetchall()
+
 
 
 def edit_command(name: str, response: str):
