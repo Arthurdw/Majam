55c67e69aa34751777d991ec99647f34afa9b8dc
Arthur De Witte <arthur.dewitte@gmail.com>
2019-11-02 23:10:30 +0100

    Server Problems are github related

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import glob
 import os
 from util.core import data, formatter
 from discord.ext import commands
-# from util.core import GitHub
+from util.core import GitHub
 
 em = formatter.embed_message
 config = configparser.ConfigParser()
@@ -31,7 +31,7 @@ class Alexi(commands.Bot):
         print('*-* Logged in as:            *-*')
         print(f'*-* Name: {self.user.name}#{self.user.discriminator}         *-*')
         print(f'*-* ID: {self.user.id}   *-*')
-        # print(f'*-* Version: {GitHub.version()}        *-*')
+        print(f'*-* Version: {GitHub.version()}        *-*')
         print('*-* *-* *-* *-*  *-* *-* *-* *-*\n')
 
     async def on_message(self, message):
--- util/core/GitHub.py
+++ util/core/GitHub.py
@@ -1,19 +1,19 @@
-# import configparser
-# from github import Github
-# 
-# # GitHub Setup
-# token = configparser.ConfigParser()
-# token.read("token.cfg")
-# g = Github(token["GIT"]["secret"])
-# 
-# # Select Alexi Repo:
-# alexi_repo = g.get_repo("Arthurdw/Alexi")
-# 
-# 
-# def version():
-#     """Get the current version (github)"""
-#     github_version = 0
-#     for _ in alexi_repo.get_commits():
-#         github_version += 1
-#     _version = str(github_version/1000).replace(".", "")
-#     return f"v {_version[:1]}.{_version[1:-2]}.{_version[2:]}"
+import configparser
+from github import Github
+
+# GitHub Setup
+token = configparser.ConfigParser()
+token.read("token.cfg")
+g = Github(token["GIT"]["secret"])
+
+# Select Alexi Repo:
+alexi_repo = g.get_repo("Arthurdw/Alexi")
+
+
+def version():
+    """Get the current version (github)"""
+    github_version = 0
+    for _ in alexi_repo.get_commits():
+        github_version += 1
+    _version = str(github_version/1000).replace(".", "")
+    return f"v {_version[:1]}.{_version[1:-2]}.{_version[2:]}"
