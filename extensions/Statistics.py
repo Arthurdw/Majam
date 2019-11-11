a0a439d4025a6aa42ef877263a1cc26523c87e20
Arthur De Witte <arthur.dewitte@gmail.com>
2019-11-11 22:57:56 +0100

    Added more in depth stats commands

--- extensions/Statistics.py
+++ extensions/Statistics.py
@@ -1,8 +1,12 @@
 import configparser
+import time
+import datetime
 from discord.ext import commands
-from util.core import data, formatter
+from util.core import data, formatter, process
 
+average_latency = []
 em = formatter.embed_message
+start = time.time()
 
 config = configparser.ConfigParser()
 config.read("config.cfg")
@@ -16,10 +20,37 @@ class Statistics(commands.Cog):
     async def stats(self, ctx):
         global_message = data.get_global_message_count()[0][0]
         global_commands = data.get_global_command_count()[0][0]
+        guilds = str(len(self.bot.guilds))
+        members = str(len(set(self.bot.get_all_members())) - 1)
+        uptime = str(datetime.timedelta(seconds=int(round(time.time() - start))))
+        first = time.perf_counter()
+        await ctx.trigger_typing()
+        last = time.perf_counter()
+        response_time = round((last-first)*1000, 2)
         await ctx.send(**em(title="Statistics:",
-                            content="**Global:**\n"
+                            content="**Bot:**\n"
+                                    f"Uptime: `{uptime}`\n"
+                                    f"Members: `{members}`\n"
+                                    f"Guilds: `{guilds}`\n"
                                     f"Messages: `{global_message}`\n"
-                                    f"Commands: `{global_commands}`"))
+                                    f"Commands: `{global_commands}`\n\n"
+                                    f"**Ping:**\n"
+                                    f"Latency: `{round(self.bot.latency * 1000, 2)}` ms\n"
+                                    f"Response Time: `{response_time}` ms\n\n"))
+
+    @commands.command(name="uptime")
+    async def uptime(self, ctx):
+        uptime = str(datetime.timedelta(seconds=int(round(time.time() - start))))
+        await ctx.send(**em(content=f"Uptime: `{uptime}`"))
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        first = time.perf_counter()
+        await ctx.trigger_typing()
+        last = time.perf_counter()
+        response_time = round((last-first)*1000, 2)
+        await ctx.send(**em(content=f"Latency: `{round(self.bot.latency * 1000, 2)}` ms\n"
+                                    f"Response Time: `{response_time}` ms\n\n"))
 
     @commands.Cog.listener()
     async def on_message(self, message):
---
+++ util/core/process.py
@@ -0,0 +1,6 @@
+def average(amounts):
+    total = len(amounts)
+    added = 0
+    for amount in amounts:
+        added += amount
+    return added/total
