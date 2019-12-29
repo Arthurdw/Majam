e1d05afdf2c029810b88816f5410bc99944b6a4f
Arthur De Witte <arthur.dewitte@gmail.com>
2019-12-29 21:11:26 +0100

    Improvement of bot version parser.

--- util/core/GitHub.py
+++ util/core/GitHub.py
@@ -7,13 +7,10 @@ token.read("token.cfg")
 g = Github(token["GIT"]["secret"])
 
 # Select Majam Repo:
-alexi_repo = g.get_repo("Arthurdw/Majam")
+majam_repo = g.get_repo("Arthurdw/Majam")
 
 
 def version():
     """Get the current version (github)"""
-    github_version = 0
-    for _ in alexi_repo.get_commits():
-        github_version += 1
-    _version = str(github_version/1000).replace(".", "")
+    github_version = majam_repo.get_commits().totalCount
     return f"v {str(github_version)[:0] or '0'}.{str(github_version)[:1] or '0'}.{str(github_version)[1:] or '0'}"
